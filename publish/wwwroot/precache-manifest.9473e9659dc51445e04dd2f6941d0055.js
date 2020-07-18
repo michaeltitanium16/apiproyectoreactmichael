9c496a6dd473bfe17e8b427f3ab98a8887af0a5e
michaeltitanium16 <michael_titanium@hotmail.com>
2020-07-18 14:42:10 -0400

    agregar carpeta donde se encuentra el front

---
+++ publish/wwwroot/precache-manifest.9473e9659dc51445e04dd2f6941d0055.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "efc752ccc86ab28024cb8ce158507ffc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0af78e62a4f7b6353fe3",
+    "url": "/static/css/2.2ce93ba3.chunk.css"
+  },
+  {
+    "revision": "3295b666b77a317c31f4",
+    "url": "/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "0af78e62a4f7b6353fe3",
+    "url": "/static/js/2.4cef0708.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.4cef0708.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3295b666b77a317c31f4",
+    "url": "/static/js/main.1e7faa09.chunk.js"
+  },
+  {
+    "revision": "eae519d9415afe73338d",
+    "url": "/static/js/runtime-main.af1c9855.js"
+  },
+  {
+    "revision": "1cedb6e919bfed6a2c1ec00b5d8ee620",
+    "url": "/static/media/UploadIcon.1cedb6e9.svg"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
